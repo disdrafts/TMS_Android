41bf8da1d61e5687576b07d8558f2dfb1c29b656
Diana <golubovskaya00@inbox.ru>
2026-07-14 19:03:08 +0300

    Changed the files

--- app/src/main/java/com/example/tms_android/MainActivity.kt
+++ app/src/main/java/com/example/tms_android/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tms_android.lesson1.MainActivityLesson1
+import com.example.tms_android.lesson2.MainActivityLesson2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,24 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btn_les1 = findViewById<Button>(R.id.btn_les1)
-        btn_les1.setOnClickListener {
+        val btnLes1 = findViewById<Button>(R.id.btn_les1)
+        btnLes1.setOnClickListener {
             val intent = Intent(this, MainActivityLesson1::class.java)
             startActivity(intent)
         }
-        val btnTask1 = findViewById<Button>(R.id.btn_task_1)
-        btnTask1.setOnClickListener {
-            val intent = Intent(this, Lesson2Task1::class.java)
-            startActivity(intent)
-        }
-        val btnTask2 = findViewById<Button>(R.id.btn_task_2)
-        btnTask2.setOnClickListener {
-            val intent = Intent(this, Task2Activity::class.java)
-            startActivity(intent)
-        }
-        val btnTask3 = findViewById<Button>(R.id.btn_task_3)
-        btnTask3.setOnClickListener {
-            val intent = Intent(this, Task3Activity::class.java)
+        val btnLes2 = findViewById<Button>(R.id.btn_les2)
+        btnLes2.setOnClickListener {
+            val intent = Intent(this, MainActivityLesson2::class.java)
             startActivity(intent)
         }
     }
